df4f6f2914504a795d646d2cecfc5235071a5fb8
Iván Baltazar Ramírez Vélez <ivanramirezvelez@gmail.com>
2025-01-02 23:42:12 +0000

    Cambios para POC 1

--- models/example/costumer_master.sql
+++
@@ -1,6 +0,0 @@
-{{config(materialized='table')}}
-
-WITH la_consulta AS 
-   (SELECT * FROM db_prueba.dbt_ivanramirezvelez.staging)
-SELECT *
-FROM la_consulta
--- models/example/my_second_dbt_model.sql
+++
@@ -1,8 +0,0 @@
-
--- Use the `ref` function to select from other models
-
-{{ config(materialized='view') }}
-
-select *
-from {{ ref('costumer_master') }}
-where profisee_id = 9999
